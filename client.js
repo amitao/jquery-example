78a5a8c410267e3d3cf269e3a576de4f0936ed14
Amie Thao <amie.c.thao@gmail.com>
2018-11-08 11:56:55 -0600

    add/delete elements targeted

--- client.js
+++ client.js
@@ -12,10 +12,20 @@ function handleReady(){
     // getter 
     console.log($('h1').text()); 
 
-    // event handler
+    // Click event Listeners
     // listening for a click - when btn is clicked display the console in handleClick() function
     // syntax - $().on('action', callback function)
     $('#submitBtn').on('click', handleClick);
+    // Listen on the parent
+    $('#output').on('click','.person', handleDelete);
+
+    // DOM Traversal
+    // clears the child of the parent 
+    $('#test').parent(); // get parent
+    $('#test').parent().children().first(); // get first children
+
+    $('#test').next(); // get you next sibling
+    $('#test').last(); // get you the last sibling
 }
 
 
@@ -25,7 +35,32 @@ function handleClick(){
     // val() is a getter/setter - can save in variable
     // const name =  $('#nameTxt').val(); // val() without parameter is getter
     const name = $('#nameTxt').val();
-    console.log('here is name', name);
-        // log what was input
+    const age = $('#ageTxt').val();
+    const food = $('#foodTxt').val();
+    console.log('here is name', name, age, food);
 
+    $('#nameTxt').val(''); 
+    $('#ageTxt').val(''); 
+    $('#foodTxt').val(''); // set the val put an empty string
+    // log what was input
+
+    // append(string of HTML you want to append) - change the element and text
+    // template literal - place holders
+    $('#output').append(`<li class="person"> ${name} is ${age} and likes ${food}</li>`);
 }
+
+function handleDelete(){
+    // $(this).remove();
+    // $(this).css('color', 'blue');
+    // $(this).addClass('highlight');
+    // $(this).removeClass('hightlight');
+    $(this).toggleClass('highlight');
+}
+
+
+
+
+
+
+
+
