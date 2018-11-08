b8258eafecfd1cd781a020f5c45972bf742c0240
Amie Thao <amie.c.thao@gmail.com>
2018-11-08 10:08:36 -0600

    corrected jquery script link

--- client.js
+++ client.js
@@ -1,1 +1,10 @@
-console.log('client is loaded');
+// console.log('client is loaded');
+
+// jquery
+// expects a callback function
+$(document).ready(handleReady);
+
+
+function handleReady(){
+    console.log('jquery loaded');
+}
